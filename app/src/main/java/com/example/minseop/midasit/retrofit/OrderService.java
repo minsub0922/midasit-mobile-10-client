c1486387289ca86816a0ca81a8eb824f7dccf630
gihwan <x1306e6d@gmail.com>
2018-05-27 05:13:39 +0900

    Apply order REST API service

--- app/src/main/java/com/example/minseop/midasit/retrofit/OrderService.java
+++ app/src/main/java/com/example/minseop/midasit/retrofit/OrderService.java
@@ -1,20 +1,50 @@
 package com.example.minseop.midasit.retrofit;
 
+import com.example.minseop.midasit.model.Order;
 import com.example.minseop.midasit.model.OrderResponse;
+import com.example.minseop.midasit.model.ResponseModel;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface OrderService {
 
-    @GET("/order/reservations")
+    @GET("/order/all")
+    Call<OrderResponse> getAllOrders();
+
+    @GET("/order/all/shoppingcart")
+    Call<OrderResponse> getAllShoppingCart();
+
+    @GET("/order/all/reservation")
     Call<OrderResponse> getAllReservations();
 
-    @GET("/order/reservation/userid/{userId}")
-    Call<OrderResponse> getAllReserationByUserId(@Path("userId") int userId);
+    @GET("/order/all/completed")
+    Call<OrderResponse> getAllCompletedOrders();
 
-    @GET("/order/completeds")
-    Call<OrderResponse> getAllCompleteds();
+    @GET("/order/all/userid/{userId}")
+    Call<OrderResponse> getAllOrdersByUserId(@Path("userId") int userId);
+
+    @GET("/order/all/shoppingcart/userid/{userId}")
+    Call<OrderResponse> getAllShoppingCartByUserId(@Path("userId") int userId);
+
+    @GET("/order/all/reservation/userid/{userId}")
+    Call<OrderResponse> getAllReservationsByUserId(@Path("userId") int userId);
+
+    @GET("/order/all/completed/userid/{userId}")
+    Call<OrderResponse> getAllCompletedOrdersByUserId(@Path("userId") int userId);
+
+    @POST("/order")
+    Call<ResponseModel> insertOrder(@Body Order order);
+
+    @PUT("/orderid/{orderId}")
+    Call<ResponseModel> updateOrder(@Path("orderId") int orderId, @Body Order order);
+
+    @DELETE("/orderid/{orderId}")
+    Call<ResponseModel> deleteOrder(@Path("orderId") int orderId);
 
 }
