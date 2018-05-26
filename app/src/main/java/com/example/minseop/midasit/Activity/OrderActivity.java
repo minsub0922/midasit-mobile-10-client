b3a057d0c3f1dfa26b6d78e5900e00ce36acb048
WON <aekik1118@naver.com>
2018-05-26 18:55:15 +0900

    shoppingcart

--- app/src/main/java/com/example/minseop/midasit/Activity/OrderActivity.java
+++ app/src/main/java/com/example/minseop/midasit/Activity/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.example.minseop.midasit.Activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,15 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 
+import com.example.minseop.midasit.MidasCafeConstants;
 import com.example.minseop.midasit.R;
+import com.example.minseop.midasit.Service.AuthService;
+import com.example.minseop.midasit.Service.ShoppingListService;
+import com.example.minseop.midasit.model.ShoppingListModel;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -20,16 +29,59 @@ public class OrderActivity extends AppCompatActivity {
     private Switch syrupSwitch;
     private Switch whippingSwitch;
 
+    private Button payNowButton;
+    private Button storeShooppingButton;
+    private int size = 0;//tall = 0, grande = 1, venti = 2
+    boolean ice = false;
+    boolean syrup = false;
+    boolean cream = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-
         setupItemImage();
         setupSizeButtons();
         setupIceSwitch();
         setupSyrupSwitch();
         setupWhippingSwitch();
+        setupButtons();
+    }
+
+    private void setupButtons() {
+        payNowButton = findViewById(R.id.order_pay_now_button);
+        storeShooppingButton = findViewById(R.id.order_store_shopping_cart_button);
+
+        payNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        storeShooppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(MidasCafeConstants.SERVER_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                final ShoppingListService ShoppingService = retrofit.create(ShoppingListService.class);
+
+                ShoppingListModel shoppingListModel = new ShoppingListModel();
+                shoppingListModel.setSize(size);
+                shoppingListModel.setCream(cream);
+                shoppingListModel.setIce(ice);
+                shoppingListModel.setSyrup(syrup);
+                final Call<ShoppingListModel> ShoppingListCall = ShoppingService.add(shoppingListModel);
+                //enqueue
+
+
+
+            }
+        });
+
     }
 
     private void setupItemImage() {
@@ -39,24 +91,42 @@ public class OrderActivity extends AppCompatActivity {
 
     private void setupSizeButtons() {
         sizeTallButton = findViewById(R.id.order_size_tall_button);
+        sizeGrandeButton = findViewById(R.id.order_size_grande_button);
+        sizeVentiButton = findViewById(R.id.order_size_venti_button);
+        sizeGrandeButton.setTextColor(Color.LTGRAY);
+        sizeVentiButton.setTextColor(Color.LTGRAY);
+        size = 0;
+
         sizeTallButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO(@gihwan)
+                size = 0;
+                sizeTallButton.setTextColor(Color.WHITE);
+                sizeGrandeButton.setTextColor(Color.LTGRAY);
+                sizeVentiButton.setTextColor(Color.LTGRAY);
             }
         });
-        sizeGrandeButton = findViewById(R.id.order_size_grande_button);
+
         sizeGrandeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO(@gihwan)
+                size = 1;
+                sizeTallButton.setTextColor(Color.LTGRAY);
+                sizeGrandeButton.setTextColor(Color.WHITE);
+                sizeVentiButton.setTextColor(Color.LTGRAY);
             }
         });
-        sizeVentiButton = findViewById(R.id.order_size_venti_button);
+
         sizeVentiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO(@gihwan)
+                size = 2;
+                sizeTallButton.setTextColor(Color.LTGRAY);
+                sizeGrandeButton.setTextColor(Color.LTGRAY);
+                sizeVentiButton.setTextColor(Color.WHITE);
             }
         });
     }
@@ -67,6 +137,14 @@ public class OrderActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton button, boolean isChecked) {
                 // TODO(@gihwan)
+                if(isChecked)
+                {
+                    ice = true;
+                }
+                else
+                {
+                    ice = false;
+                }
             }
         });
     }
@@ -77,6 +155,14 @@ public class OrderActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton button, boolean isChecked) {
                 // TODO(@gihwan)
+                if(isChecked)
+                {
+                    syrup = true;
+                }
+                else
+                {
+                    syrup = false;
+                }
             }
         });
     }
@@ -87,7 +173,18 @@ public class OrderActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton button, boolean isChecked) {
                 // TODO(@gihwan)
+                if(isChecked)
+                {
+                    cream = true;
+                }
+                else
+                {
+                    cream = false;
+                }
             }
         });
     }
+
+
+
 }
