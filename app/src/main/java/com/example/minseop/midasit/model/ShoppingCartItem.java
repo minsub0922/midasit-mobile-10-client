3f43c5ad10442f404ce0f68aa25b86e4660231a7
WON <aekik1118@naver.com>
2018-05-26 20:56:05 +0900

    shoppingCartService

--- app/src/main/java/com/example/minseop/midasit/model/ShoppingCartItem.java
+++ app/src/main/java/com/example/minseop/midasit/model/ShoppingCartItem.java
@@ -4,19 +4,28 @@ package com.example.minseop.midasit.model;
  * Created by WON on 2018-05-26.
  */
 
-public class ShoppingCartModel extends ResponseModel {
-    private int id;
+public class ShoppingCartItem extends ResponseModel {
+    private int menuId;
     private int size;
     private boolean ice;
     private boolean syrup;
-    private boolean cream;
+    private boolean whipping;
+    private int count;
 
-    public int getId() {
-        return id;
+    public int getCount() {
+        return count;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
     }
 
     public int getSize() {
@@ -43,24 +52,24 @@ public class ShoppingCartModel extends ResponseModel {
         this.syrup = syrup;
     }
 
-    public boolean isCream() {
-        return cream;
+    public boolean isWhipping() {
+        return whipping;
     }
 
-    public void setCream(boolean cream) {
-        this.cream = cream;
+    public void setWhipping(boolean whipping) {
+        this.whipping = whipping;
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("ShoppingCartModel{");
+        final StringBuilder sb = new StringBuilder("ShoppingCartItem{");
         sb.append("time=").append(time);
         sb.append(", success=").append(success);
-        sb.append(", id=").append(id);
+        sb.append(", menuId=").append(menuId);
         sb.append(", size=").append(size);
         sb.append(", ice='").append(ice);
         sb.append(", syrup='").append(syrup);
-        sb.append(", cream=").append(cream);
+        sb.append(", whipping=").append(whipping);
         sb.append('}');
         return sb.toString();
     }
