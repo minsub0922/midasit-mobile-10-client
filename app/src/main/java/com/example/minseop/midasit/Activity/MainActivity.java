6a1c780fe4a27461ecf3d09a185309eb245151c7
minseop <minsub0922@naver.com>
2018-05-26 17:40:01 +0900

    App Theme Change

--- app/src/main/java/com/example/minseop/midasit/Activity/MainActivity.java
+++ app/src/main/java/com/example/minseop/midasit/Activity/MainActivity.java
@@ -5,10 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
--- app/src/main/java/com/example/minseop/midasit/fragment/MenuListFragment.java
+++ app/src/main/java/com/example/minseop/midasit/fragment/MenuListFragment.java
@@ -1,5 +1,6 @@
 package com.example.minseop.midasit.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,7 +9,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.minseop.midasit.Activity.MenuDetailActivity;
 import com.example.minseop.midasit.Item.MenuItem;
 import com.example.minseop.midasit.R;
 import com.example.minseop.midasit.adapter.MenuItemRecyclerAdapter;
@@ -19,35 +22,84 @@ import java.util.List;
  * Created by minseop on 2018-05-26.
  */
 
-public class MenuListFragment extends Fragment {
-    private MenuItemRecyclerAdapter adapter;
-    private List<MenuItem> data = new ArrayList<>();
-    private RecyclerView recyclerView;
+public class MenuListFragment extends Fragment implements View.OnClickListener{
+    private MenuItemRecyclerAdapter adapter1, adapter2, adapter3;
+    private List<MenuItem> data1 = new ArrayList<>(),  data2 = new ArrayList<>(),  data3 = new ArrayList<>();
+    private RecyclerView recyclerView1, recyclerView2, recyclerView3;
     private static final String TAG = "Success????";
+    TextView more1, more2, more3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_menulist,null);
-        Log.d(TAG, "done???");
+        View view = inflater.inflate(R.layout.fragment_menulist,container, false);
+        Log.d(TAG, String.valueOf(getActivity()));
 
-        LinearLayoutManager layoutManager
+        more1 = view.findViewById(R.id.more1);
+        more2 = view.findViewById(R.id.more2);
+        more3 = view.findViewById(R.id.more3);
+
+        more1.setOnClickListener(this);
+        more2.setOnClickListener(this);
+        more3.setOnClickListener(this);
+
+        LinearLayoutManager layoutManager1
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager layoutManager2
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager layoutManager3
                 = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
-        adapter = new MenuItemRecyclerAdapter(getActivity(), data);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(layoutManager);
+        recyclerView1 = (RecyclerView) view.findViewById(R.id.recyclerview1);
+        adapter1 = new MenuItemRecyclerAdapter(getActivity(), data1);
+        recyclerView1.setAdapter(adapter1);
+        recyclerView1.setLayoutManager(layoutManager1);
+
+        recyclerView2 = (RecyclerView) view.findViewById(R.id.recyclerview2);
+        adapter2 = new MenuItemRecyclerAdapter(getActivity(), data2);
+        recyclerView2.setAdapter(adapter2);
+        recyclerView2.setLayoutManager(layoutManager2);
+
+        recyclerView3 = (RecyclerView) view.findViewById(R.id.recyclerview3);
+        adapter3 = new MenuItemRecyclerAdapter(getActivity(), data3);
+        recyclerView3.setAdapter(adapter3);
+        recyclerView3.setLayoutManager(layoutManager3);
+
+
+
 
         for (int i=0; i<5; i++){
             MenuItem item = new MenuItem("",  "아메리카노"+i+"번", 3000+1000*i);
-            data.add(item);
+            data1.add(item);
+        }
+        for (int i=0; i<5; i++){
+            MenuItem item = new MenuItem("",  "아메리카노"+i+"번", 3000+1000*i);
+            data2.add(item);
         }
 
-        adapter.notifyDataSetChanged();
+        for (int i=0; i<5; i++){
+            MenuItem item = new MenuItem("",  "아메리카노"+i+"번", 3000+1000*i);
+            data3.add(item);
+        }
+
+        adapter1.notifyDataSetChanged();
+        adapter2.notifyDataSetChanged();
+        adapter3.notifyDataSetChanged();
 
         return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getActivity(), MenuDetailActivity.class);
+        if (v.getId()==R.id.more1){
+            intent.putExtra("title","Coffee");
+        }else if(v.getId() ==R.id.more2){
+            intent.putExtra("title","Tea");
+        }else if (v.getId() == R.id.more3){
+            intent.putExtra("title","Beverage");
+        }
+        startActivity(intent);
+    }
 }
