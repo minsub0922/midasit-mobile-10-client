52a4926d608df19256352e970ad83512b02a4b54
gihwan <x1306e6d@gmail.com>
2018-05-26 15:33:04 +0900

    Fix fragment container at MainActivity

--- app/src/main/java/com/example/minseop/midasit/Activity/MainActivity.java
+++ app/src/main/java/com/example/minseop/midasit/Activity/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.minseop.midasit.Activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -8,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.minseop.midasit.R;
+import com.example.minseop.midasit.fragment.MenuListFragment;
 
 /**
  * Created by minseop on 2018-05-25.
@@ -17,12 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private BottomNavigationView bottomNavigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setupAppBar();
+        setupBottomNavigation();
+
+        replaceViewPager(new MenuListFragment());
     }
 
     @Override
@@ -44,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private boolean replaceViewPager(@NonNull Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_fragment_container, fragment)
+                .commit();
+        return true;
+    }
+
     private void setupAppBar() {
         Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
@@ -51,6 +69,26 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+    }
+
+    private void setupBottomNavigation() {
+        bottomNavigation = findViewById(R.id.main_bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                final int itemId = item.getItemId();
+
+                if (itemId == R.id.bottom_navigation_action_home) {
+                    return replaceViewPager(new MenuListFragment());
+                } else if (itemId == R.id.bottom_navigation_action_menu) {
+                    return replaceViewPager(new MenuListFragment());
+                } else if (itemId == R.id.bottom_navigation_action_mypage) {
+                    return replaceViewPager(new MenuListFragment());
+                }
+
+                return false;
+            }
+        });
     }
 
 }
