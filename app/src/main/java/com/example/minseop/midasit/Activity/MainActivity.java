dc608c51eb4e0be65539c180086731597fbe0d9f
gihwan <x1306e6d@gmail.com>
2018-05-26 13:27:37 +0900

    Create MainActivity with Toolbar and BottomNavigationView

--- app/src/main/java/com/example/minseop/midasit/Activity/MainActivity.java
+++ app/src/main/java/com/example/minseop/midasit/Activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.minseop.midasit.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.minseop.midasit.R;
 
@@ -11,10 +14,43 @@ import com.example.minseop.midasit.R;
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, TestActivity.class));
+
+        setupAppBar();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int itemId = item.getItemId();
+
+        if (itemId == R.id.toolbar_action_notification) {
+            return true;
+        } else if (itemId == R.id.toolbar_action_shopping_cart) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupAppBar() {
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
 }
